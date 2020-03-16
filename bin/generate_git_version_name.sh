93d7209a1dee544072ddfd14a6e6d0a1237e7c06
Jurjan Woltman <jurjan.woltman@gmail.com>
2020-03-16 11:13:30 +0100

    Moved pipeline file, added bin to generate version number

---
+++ bin/generate_git_version_name.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -eu
+
+count=$(git rev-list --all --count)
+branch=$(git show-ref | grep $(git log --pretty=%h -1) | sed 's|.*/\(.*\)|\1|' | sort -u | grep -v HEAD)
+hash=$(git rev-parse --short HEAD)
+
+branchName="${CI_COMMIT_REF_SLUG:-${branch/\//-}}"
+gitVersion="${count}-${hash}-${branchName}"
+
+echo "versionName=${gitVersion}"
+echo "branchName=${branchName}"
